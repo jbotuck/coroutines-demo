b5c783efa34519f03de2b43dcad6d0623c907be4
Jacob Botuck <jacob.botuck@powerreviews.com>
2022-07-24 11:08:38 -0400

    use chunking

--- src/main/kotlin/com/example/demo/runner/Runner.kt
+++ src/main/kotlin/com/example/demo/runner/Runner.kt
@@ -11,18 +11,24 @@ import org.springframework.stereotype.Component
 @Component
 class Runner(val mongoTemplate: MongoTemplate, val kafkaTemplate: KafkaTemplate<String, Any>) : CommandLineRunner {
     override fun run(vararg args: String?) {
+        val chunkSize = args.first()?.toInt() ?: 50
         val query = Query()// some query that pulls a lot of results
 
-
-        mongoTemplate.stream<Widget>(query).use {
-            for (widget in it) {
-
-
-                //transformation logic might go here
-
-
-                kafkaTemplate.sendDefault(widget)
-            }
+        mongoTemplate.stream<Widget>(query).use { records ->
+            records
+                .asSequence()
+                .map {
+                    //transformation logic might go here
+                    it
+                }
+                .chunked(chunkSize)
+                .forEach { records ->
+                    val results = records.map {
+                        kafkaTemplate.sendDefault(it)
+                    }
+                    kafkaTemplate.flush()
+                    for (result in results) result.get()
+                }
         }
     }
 }

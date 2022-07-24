ffe90d5d3b166789d412478528467d6b4ea03a77
Jacob Botuck <jacob.botuck@powerreviews.com>
2022-07-24 10:55:51 -0400

    use stream

--- src/main/kotlin/com/example/demo/runner/Runner.kt
+++ src/main/kotlin/com/example/demo/runner/Runner.kt
@@ -3,8 +3,8 @@ package com.example.demo.runner
 import com.example.demo.model.Widget
 import org.springframework.boot.CommandLineRunner
 import org.springframework.data.mongodb.core.MongoTemplate
-import org.springframework.data.mongodb.core.find
 import org.springframework.data.mongodb.core.query.Query
+import org.springframework.data.mongodb.core.stream
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Component
 
@@ -13,7 +13,7 @@ class Runner(val mongoTemplate: MongoTemplate, val kafkaTemplate: KafkaTemplate<
     override fun run(vararg args: String?) {
         val query = Query()// some query that pulls a lot of results
 
-        for (widget in mongoTemplate.find<Widget>(query)) {
+        for (widget in mongoTemplate.stream<Widget>(query)) {
 
 
             //transformation logic might go here

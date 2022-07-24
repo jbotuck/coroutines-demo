276e23013e7d9a839cf7b5863bde25e5b3646981
Jacob Botuck <jacob.botuck@powerreviews.com>
2022-07-24 10:49:27 -0400

    naive approach

---
+++ src/main/kotlin/com/example/demo/model/Widget.kt
@@ -0,0 +1,5 @@
+package com.example.demo.model
+
+data class Widget(
+    val x: Int, val y: Int
+)
---
+++ src/main/kotlin/com/example/demo/runner/Runner.kt
@@ -0,0 +1,25 @@
+package com.example.demo.runner
+
+import com.example.demo.model.Widget
+import org.springframework.boot.CommandLineRunner
+import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.find
+import org.springframework.data.mongodb.core.query.Query
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.stereotype.Component
+
+@Component
+class Runner(val mongoTemplate: MongoTemplate, val kafkaTemplate: KafkaTemplate<String, Any>) : CommandLineRunner {
+    override fun run(vararg args: String?) {
+        val query = Query()// some query that pulls a lot of results
+
+        for (widget in mongoTemplate.find<Widget>(query)) {
+
+
+            //transformation logic might go here
+
+
+            kafkaTemplate.sendDefault(widget)
+        }
+    }
+}
